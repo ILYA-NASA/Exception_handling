7242d2b42e2e0f097091640189fd61ec0deea3b6
ILYA.NASA <il.nasa@outlook.com>
2022-09-26 22:57:57 +0300

    added try with recurse and Override toString

--- src/home03/Task01.java
+++ src/home03/Task01.java
@@ -1,5 +1,6 @@
 package home03;
 
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Task01 {
@@ -7,26 +8,34 @@ public class Task01 {
     String lastName;
     String thirdName;
     String birthDate;
-    int telephoneNum;
     String gender;
+    int telephoneNum;
 
-    public Task01(String firstName, String lastName, String thirdName, String birthDate, int telephoneNum, String gender) {
+    public Task01(String firstName, String lastName, String thirdName, String birthDate, String gender, int telephoneNum) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.thirdName = thirdName;
         this.birthDate = birthDate;
-        this.telephoneNum = telephoneNum;
         this.gender = gender;
+        this.telephoneNum = telephoneNum;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input: your first name, your last name, your third name, your birth date, " +
-                "your telephone number and your gender");
-        try {
-            Task01 user = new Task01(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextLine());
+        System.out.println("Please input: your first name, your last name, your third name, your birth date, "
+                + "your gender and your telephone number");
+        try (FileWriter writer = new FileWriter("UserData")) {
+            Task01 user = new Task01(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(),
+                    sc.nextLine(), sc.nextInt());
+            writer.write(user.toString());
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Invalid input!");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "<" + lastName + "><" + firstName + "><" + thirdName + "><"
+                + birthDate + "><" + gender + "><" + telephoneNum + '>';
     }
 }
